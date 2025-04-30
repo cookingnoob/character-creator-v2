2ddfccaeaee42fa62f631f6a10c509b47856e87a
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-04-30 17:38:14 -0600

    add rest of styles

--- src/components/CharacterEditor/CharacterEditor.jsx
+++ src/components/CharacterEditor/CharacterEditor.jsx
@@ -29,7 +29,19 @@ function App() {
   );
 
   return (
+    <>    
+    <div className={styles.backgroundDecorative}></div>
     <main className={styles.characterEditor}>
+            <div className={styles.characterWrapper}>
+        <Character
+          body={body}
+          head={head}
+          face={face}
+          accessory={accessory}
+          skinColor={skinColor}
+          clothesColor={clothesColor}
+        />
+      </div>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
@@ -78,17 +90,11 @@ function App() {
         </div>
       </MaxWidthWrapper>
 
-      <div className={styles.characterWrapper}>
-        <Character
-          body={body}
-          head={head}
-          face={face}
-          accessory={accessory}
-          skinColor={skinColor}
-          clothesColor={clothesColor}
-        />
-      </div>
+
+     
     </main>
+    </>
+
   );
 }
 
