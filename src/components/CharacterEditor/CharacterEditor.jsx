e10d5bf3043db1144d0ebf0958a93a78a5359ee9
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-04-30 18:25:44 -0600

    update styles according to some of the videos answers

--- src/components/CharacterEditor/CharacterEditor.jsx
+++ src/components/CharacterEditor/CharacterEditor.jsx
@@ -32,7 +32,8 @@ function App() {
     <>    
     <div className={styles.backgroundDecorative}></div>
     <main className={styles.characterEditor}>
-            <div className={styles.characterWrapper}>
+    
+      <div className={styles.characterWrapper}>
         <Character
           body={body}
           head={head}
@@ -43,6 +44,7 @@ function App() {
         />
       </div>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
+        
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
